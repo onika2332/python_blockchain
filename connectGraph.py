ec70c5f1606149eafdb08baac6b69ade0c81979f
namnam <namnam@coder.com>
2021-05-13 03:39:10 +0700

    modify the add_one_node() to add any type of node, add some search function

--- connectGraph.py
+++ connectGraph.py
@@ -17,9 +17,9 @@ class MyGraph:
     
     #add one node to graph
     @classmethod
-    def add_one_node(self, **kwargs):
+    def add_one_node(self, node_type="Wallet", **kwargs):
         # CREATE statement
-        msg = "create "
+        msg = "create (n:{}".format(node_type) + "{"
 
         # if import data is none   
         assert kwargs is not None, "no data to import"
@@ -27,19 +27,18 @@ class MyGraph:
         count = 0 
         for key,value in kwargs.items():
             if count < len(kwargs) - 1:
-                if 0 == count:
-                    msg += ("(n:{}".format(value) + "{")
+                if(0 == count):
+                    msg+= '{}:"{}",'.format(key,value)
                 else:
                     msg += "{}:{},".format(key,value)
                 count += 1 
             else:
                 msg += ("{}:{}".format(key,value) + "})")
-        (msg)
         #print(msg)
         self.session.run(msg)
         
     @classmethod
-    def add_node_from_json(self, filename1='acc.json',filename12='fico1.json'):
+    def add_wallet_node_from_json(self, filename1='acc.json',filename12='fico1.json'):
         f1 = open('acc.json')
         f2 = open('fico1.json')
 
@@ -48,7 +47,7 @@ class MyGraph:
 
         info = {}
         info["id"] = []
-        info["label"] = []
+        info["address"] = []
         info["average"] = []
         info["balance"] = []
         info["history"] = []
@@ -56,7 +55,7 @@ class MyGraph:
         
         for key, value in data1.items():    
             info["id"].append(key)
-            info["label"].append(value)
+            info["address"].append(value)
 
         for obj in data2:
             info["average"].append(obj["average_transaction"])
@@ -67,17 +66,75 @@ class MyGraph:
         for i in range(0,len(info["id"])):
             if i < len(info["fico"]):
                 self.add_one_node(
-                                label = "N" + str(info["label"][i]),
-                                id = str(info["id"][i]),
-                                average = str(info["average"][i]),
-                                balance = str(info["balance"][i]),
-                                history = str(info["history"][i]),
-                                fico = str(info["fico"][i]))
+                                address = info["address"][i],
+                                id = info["id"][i],
+                                average = info["average"][i],
+                                balance = info["balance"][i],
+                                history = info["history"][i],
+                                fico = info["fico"][i])
             else:
-                self.add_one_node(label = "N" + str(info["label"][i]),
-                                    id = str(info["id"][i]))
+                self.add_one_node(label = info["address"][i],
+                                    id = info["id"][i])
+    
+    @classmethod
+    def search_by_address(self, address):
+        msg = 'MATCH (n) WHERE n.address = "{}" RETURN (n)'.format(hex(address))
 
+        data = self.session.run(msg).data()
+        
+        if data is not None:
+            return data
+        else:
+            print("This address isn't available")
+
+    @classmethod
+    def search_by_balance(self, balance_value):
+        count_msg = "MATCH (n:{}) WHERE n.balance >= {} RETURN(n)".format("Wallet",balance_value)
+        
+        accounts = self.session.run(count_msg)
+        
+        list_wallets = [acc for acc in accounts.data()]
+        
+        print(len(list_wallets))
+        if len(list_wallets) != 0 :
+            return list_wallets
+        
+        print("No wallets reach this balance value {}".format(balance_value))
+
+    @classmethod
+    def seacrh_by_fico(self, fico):
+        count_msg = "MATCH (n:{}) WHERE n.fico >= {} RETURN(n)".format("Wallet",fico)
+        
+        accounts = self.session.run(count_msg)
+        
+        list_wallets = [acc for acc in accounts.data()]
+        
+        if len(list_wallets) != 0:
+            return list_wallets
+        
+        print("No wallets reach this fico {}".format(fico))
+    
+    @classmethod
+    def delete_node(self, *args):
+        #delete one or multiple node
+        assert args is not None, "No address specified for deletion"
+
+        for address in args:
+            msg = 'MATCH(n) WHERE n.address = "{}" DETACH DELETE(n)'.format(hex(address))
+            self.session.run(msg)
+    
+    # @classmethod
+    # def add_relationship(type)
+
+    # @classmethod
+    # def search_30_latest_transactions(address):
+        
 if __name__ == '__main__':
     new_graph = MyGraph("bolt://localhost:7687","neo4j","3.14159265")
-    new_graph.add_node_from_json()
+    #new_graph.add_wallet_node_from_json()
+    
+    #print(new_graph.search_by_address(0x788cabe9236ce061e5a892e1a59395a81fc8d62c))
+    #print(new_graph.search_by_balance(250))
+    print(new_graph.search_by_fico(100))
     new_graph.close()
+
--- test_concat_string.py
+++ test_concat_string.py
@@ -1,6 +1,12 @@
-s1 = "abc"
-s2 = "\""
-s3 = 234
-data = {"abc" : "xyz"}
-for key, value in data.items():
-    print(f"Result is {key} : {value}" + "helloooo")
+from neo4j import GraphDatabase as gr
+
+
+adds = str(0xd80956d9c0b6fa21c9eaa4d4cfd4742d9294356fc1b4f715e6048ef9dc760f14)
+print(adds, type(adds))
+add = "address:{}".format(adds)
+query4 = "create (n:{}".format("player100") + "{" + "{},".format(add) + "team: juventus})"
+print(query4)
+
+session = gr.driver(uri="bolt://localhost:7687",auth=("neo4j","3.14159265")).session()
+session.run(query4)
+
